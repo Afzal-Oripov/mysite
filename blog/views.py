04bf4807c8dccf11d7b954c6882953c702dc0916
Afzal <afzaloripov007@gmail.com>
2024-06-14 20:42:38 +0500

    Added a static CSS file

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 # Create your views here.
 
+
+
 def post_list(request):
     posts = Post.published.all()
     context = {
-        'posts' : posts
+        'posts': posts,
     }
-    return render(request, 'blog/posts/list.html', context)
+    return render(request, 'blog/post/list.html', context)
+
+
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
-    return render(request, 'blog/posts/detail.html',{'post': post})
+    post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED) 
+    return render(request, 'blog/post/detail.html',{'post': post})
