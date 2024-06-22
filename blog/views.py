be05f0a632c916aea8a781885b890c9edb5fba48
Afzal <afzaloripov007@gmail.com>
2024-06-22 16:59:07 +0500

    fixed buds

--- blog/views.py
+++ blog/views.py
@@ -15,10 +15,11 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED, slug=post,
-publish__year=year,
-publish__month=month,
-publish__day=day) 
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, 
+                             slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day) 
     context = {
         'post': post
     }
