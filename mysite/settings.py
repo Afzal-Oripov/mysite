387bcc67236521e1cbf8dbed87b8e8889f6620ec
Afzal <afzaloripov007@gmail.com>
2024-06-28 13:32:00 +0500

    fixed email

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'afzaloripov007@gmail.com'
 EMAIL_HOST_PASSWORD = 'vuym diwz ejjc pmeb'
