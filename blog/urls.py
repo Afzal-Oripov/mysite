c686329060190897e9b28f9dad9241b5dba95f6b
Elyor548 <mansurovelyor08@gmail.com>
2024-06-29 18:46:16 +0300

    Send Emails

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import post_list, post_detail, PostListView
+from . views import post_list, post_detail, post_share
 
 app_name = 'blog'
 
 urlpatterns = [
- # представления поста
- path('', post_list, name='post_list'),
- path('<int:post_id>share/', post_detail, name='post_detail')
+    # представления поста
+    path('', post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share'),
 ]
