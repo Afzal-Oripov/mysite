7948f73aa1931c39c4e35578a6caa95959a1509b
Afzal <afzaloripov007@gmail.com>
2024-07-02 21:00:21 +0500

    added comments

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
 from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
 
+
 class PublishedManager(models.Manager): 
     def get_queryset(self):
-        return super().get_queryset()\
-            .filter(status=Post.Status.PUBLISHED)
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
 
@@ -25,8 +25,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
-    objects = models.Manager() # менеджер, применяемый по умолчанию
+    objects = models.Manager() # менеджер, применяемый по умолчанию 
     published = PublishedManager() # конкретно-прикладной менеджер
+
 
     class Meta:
         ordering = ['-publish']
@@ -36,9 +37,25 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month,self.publish.day,self.slug])
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [ models.Index(fields=['created']),]
 
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
-
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 
 
 class PostListView(ListView):
-
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 2
     template_name = 'blog/post/list.html'
+
 
 def post_list(request):
     post = Post.published.all()
@@ -22,13 +22,14 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1) 
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
+
     context = {
         'posts': posts,
-    }
+        }
     return render(request, 'blog/post/list.html', context)
 
 
@@ -39,10 +40,11 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True) 
     form = CommentForm()
     context = {
-        'post': post
+        'post': post,
+        'comments': comments, 
+        'form': form
         }
     return render(request, 'blog/post/detail.html', context)
-
 
 
 
@@ -55,6 +57,25 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri( post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'imowww1234@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST) 
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
