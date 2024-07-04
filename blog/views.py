024592d77c0aa52b92243d74cd8e9db5c8b22512
Elyor548 <mansurovelyor08@gmail.com>
2024-07-04 18:30:58 +0300

    something added

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 # Create your views here.
 
 
@@ -16,8 +17,12 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request,tag_slug=None):
     post = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post, 2)
     page_number = request.GET.get('page', 1)
     try:
@@ -30,7 +35,8 @@ def post_list(request):
     context = {
         'posts': posts,
         }
-    return render(request, 'blog/post/list.html', context)
+    
+    return render( request, 'blog/post/list.html', context, )
 
 
 
