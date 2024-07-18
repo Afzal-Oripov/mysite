36d1c38a270073d1a8f8e92d3928e378e9aa0e5b
Afzal <afzaloripov007@gmail.com>
2024-07-18 17:59:24 +0500

    last commit

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('<int:post_id>/comment/', post_comment, name='post_comment'),
     path('', views.post_list, name='post_list'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('search/', views.post_search, name='post_search'),
     
 ]
